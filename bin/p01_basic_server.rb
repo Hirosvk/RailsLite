26eb6a3ba4e44633454fb434493b6bf0af99ef5c
App Academy Student <student@appacademy.io>
2016-06-07 17:53:47 -0700

    halfway done

---
+++ bin/p01_basic_server.rb
@@ -0,0 +1,23 @@
+require 'rack'
+
+  loveaa = Proc.new do |env|
+    req = Rack::Request.new(env)
+    res = Rack::Response.new
+    p req.path
+    p req.request_method
+    p req.params
+    if req.path =~ /^\/i\/love\/appacademy/
+      res.write("I love App Academy")
+    else
+      res.write("I guess you don't love App Academy")
+    end
+    res['Content-Type'] = 'text/html'
+    res.finish
+  end
+
+
+Rack::Server.start(
+  app: loveaa,
+
+  Port: 8080
+)
